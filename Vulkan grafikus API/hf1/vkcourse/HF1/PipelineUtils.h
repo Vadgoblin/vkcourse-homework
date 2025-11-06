b8996ca49059feb8771550c79e968ed27f4dae5c
vadgoblin@arch <vadgoblin@github.com>
2025-11-06 14:23:54 +0100

    refactor pipeline creation

---
+++ Vulkan grafikus API/hf1/vkcourse/HF1/PipelineUtils.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vulkan/vulkan.h>
+
+VkPipelineLayout CreateEmptyPipelineLayout(VkDevice device, uint32_t pushConstantSize);
+VkPipeline CreateSimplePipeline(VkDevice device,
+                                VkFormat colorFormat,
+                                VkPipelineLayout layout,
+                                VkShaderModule vertShader,
+                                VkShaderModule fragShader);
