e9941f924d98f9cc3b3b03f0cfd4a5efac508343
vadgoblin@arch <vadgoblin@github.com>
2025-11-07 16:36:48 +0100

    move shaders into a dedicated directory

--- shader.cmake
+++ shader.cmake
@@ -4,20 +4,25 @@ function(add_shader SHADER_TARGET SHADER_FILE SHADER_VAR_NAME)
     set(SHADER_INPUT ${CMAKE_CURRENT_SOURCE_DIR}/${SHADER_FILE})
     set(SHADER_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${SHADER_FILE}_include.h)
 
+    get_filename_component(SHADER_NAME ${SHADER_FILE} NAME)
+
     # Create command which compiles the shader
     add_custom_command(OUTPUT ${SHADER_OUTPUT}
-        DEPENDS ${SHADER_INPUT}
-        COMMAND ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}
-                -V
-                --variable-name ${SHADER_VAR_NAME}
-                -I${CMAKE_CURRENT_SOURCE_DIR}
-                ${SHADER_INPUT}
-                -o ${SHADER_OUTPUT}
+            DEPENDS ${SHADER_INPUT}
+            COMMAND ${Vulkan_GLSLANG_VALIDATOR_EXECUTABLE}
+            -V
+            --variable-name ${SHADER_VAR_NAME}
+            -I${CMAKE_CURRENT_SOURCE_DIR}
+            ${SHADER_INPUT}
+            -o ${SHADER_OUTPUT}
+            COMMENT "Compiling shader: ${SHADER_FILE}"
     )
-    add_custom_target(${SHADER_TARGET}-spv-${SHADER_FILE} DEPENDS ${SHADER_OUTPUT})
-    add_dependencies(${SHADER_TARGET} ${SHADER_TARGET}-spv-${SHADER_FILE})
+
+    add_custom_target(${SHADER_TARGET}-spv-${SHADER_NAME} DEPENDS ${SHADER_OUTPUT})
+
+    add_dependencies(${SHADER_TARGET} ${SHADER_TARGET}-spv-${SHADER_NAME})
     target_include_directories(${SHADER_TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
-endfunction(add_shader)
+endfunction()
 
 function(add_shaders SHADER_TARGET)
     list(LENGTH ARGV ARG_COUNT)
