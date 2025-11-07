e5e5fef2eeba6666a331384534fbecaa9b607abc
vadgoblin@arch <vadgoblin@github.com>
2025-11-07 17:50:07 +0100

    add new cube to the scene

--- HF1/ObjectManager.cpp
+++ HF1/ObjectManager.cpp
@@ -10,23 +10,29 @@ std::vector<BasePrimitive*> objects;
 
 void SetupAll(const Context& context, const Swapchain& swapchain, size_t pushConstansStart)
 {
+    Grid* grid = new Grid(2,false);
+    grid->Create(context, swapchain.format(), pushConstansStart);
+    grid->setRotation(90.0f, 0.0f, 0.0f);
+    grid->setScale(12.0f, 12.0f, 0.0f);
+    objects.push_back(grid);
+
     Cube* cube = new Cube(true);
     cube->setPosition(0.0f, 0.45f,0.0f);
     cube->setScale(1.0f,0.9f,1.0f);
-    cube->setRotation(15.0f,0.0f,0.0f);
     cube->Create(context, swapchain.format(), pushConstansStart);
     objects.push_back(cube);
 
-    Cylinder* cylinder = new Cylinder(0.2, 0.2, 1, 25, 2, true);
-    cylinder->setPosition(0.0f, 1.0f,0.0f);
+    Cylinder* cylinder = new Cylinder(0.05, 0.05, 1, 25, 2, true);
+    cylinder->setPosition(0.0f, 0.5f,0.0f);
+    cylinder->setRotation(90.0f,0.0f,0.0f);
     cylinder->Create(context, swapchain.format(), pushConstansStart);
     objects.push_back(cylinder);
 
-    Grid* grid = new Grid(2,true);
-    grid->Create(context, swapchain.format(), pushConstansStart);
-    grid->setRotation(90.0f, 0.0f, 0.0f);
-    grid->setScale(8.0f, 8.0f, 0.0f);
-    objects.push_back(grid);
+    Cube* cube2 = new Cube(true);
+    cube2->setPosition(0.0f, 0.95f,0.0f);
+    cube2->setScale(1.0f,0.1f,1.0f);
+    cube2->Create(context, swapchain.format(), pushConstansStart);
+    objects.push_back(cube2);
 }
 
 void DrawAll(VkCommandBuffer cmd)
